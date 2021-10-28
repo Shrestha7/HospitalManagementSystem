229d761d522685744b1989a51e61515e54612d4e
SWASTIK SHRESTHA <SWASTIKS@nibl.com.np>
2021-10-28 14:53:54 +0545

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { PatientModule } from './app/Patient/Patient.PatientModule';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(PatientModule)
   .catch(err => console.error(err));
